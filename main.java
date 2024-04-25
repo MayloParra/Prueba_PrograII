0a6f3269d1b17283e1e2b2a23651de05c31a2369
CesarCM19 <cesarcamposmora04@gmail.com>
2024-04-24 18:48:36 -0600

    JOption

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class main{
     public static void main(String[] args) {
         System.out.println("Hola");
+        JOptionPane.showMessageDialog(null,"Hola");
     }
 }
